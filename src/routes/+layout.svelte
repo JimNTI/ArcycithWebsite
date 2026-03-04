8e59d8c2769e0950c4a3924009eca842e8fed082
JimNTI <jim.palo@elev.ga.ntig.se>
2026-03-04 09:10:44 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,62 @@
+<script>
+	import Header from './Header.svelte';
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		{@render children()}
+		{@render children()}
+		{@render children()}
+	</main>
+
+	<footer>
+		<p>
+			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
+		</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 80%;
+		min-height: 110vw;
+		margin: 0 auto;
+		box-sizing: border-box;
+		outline: solid 1px black;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight:lighter;
+		font-size: 5px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
